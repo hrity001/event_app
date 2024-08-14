a744522641a0240a09da872e8c6b0a435c8e7034
hritysahu12 <118751557+hritysahu12@users.noreply.github.com>
2024-08-14 16:09:09 +0530

    first commit

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -8,22 +8,19 @@ import { Input } from "@/components/ui/input"
 import { eventFormSchema } from "@/lib/validator"
 import * as z from 'zod'
 import { eventDefaultValues } from "@/constants"
-
+import Dropdown from "./Dropdown"
 import { Textarea } from "@/components/ui/textarea"
 import { FileUploader } from "./FileUploader"
 import { useState } from "react"
-import Dropdown from "./Dropdown"
 import Image from "next/image"
-import DatePicker from 'react-datepicker';
-import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from "react-datepicker";
+import { useUploadThing } from '@/lib/uploadthing'
 
+import "react-datepicker/dist/react-datepicker.css";
 import { Checkbox } from "../ui/checkbox"
 import { useRouter } from "next/navigation"
-import { useUploadThing } from '@/lib/uploadthing'
-import { IEvent } from "@/lib/database/models/event.model"
 import { createEvent, updateEvent } from "@/lib/actions/event.actions"
-
-
+import { IEvent } from "@/lib/database/models/event.model"
 
 
 type EventFormProps = {
@@ -31,104 +28,107 @@ type EventFormProps = {
   type: "Create" | "Update"
   event?: IEvent,
   eventId?: string
-  
 }
 
 const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
-    const [files, setFiles] = useState<File[]>([])
-    const initialValues = eventDefaultValues;
-    const router = useRouter();
-  
-    const { startUpload } = useUploadThing('imageUploader')
-  
-    const form = useForm<z.infer<typeof eventFormSchema>>({
-      resolver: zodResolver(eventFormSchema),
-      defaultValues: initialValues
-    })
-   
-    async function onSubmit(values: z.infer<typeof eventFormSchema>) {
-      let uploadedImageUrl = values.imageUrl;
-  
-      if(files.length > 0) {
-        const uploadedImages = await startUpload(files)
-  
-        if(!uploadedImages) {
-          return
-        }
-  
-        uploadedImageUrl = uploadedImages[0].url
+  const [files, setFiles] = useState<File[]>([])
+  const initialValues = event && type === 'Update' 
+    ? { 
+      ...event, 
+      startDateTime: new Date(event.startDateTime), 
+      endDateTime: new Date(event.endDateTime) 
+    }
+    : eventDefaultValues;
+  const router = useRouter();
+
+  const { startUpload } = useUploadThing('imageUploader')
+
+  const form = useForm<z.infer<typeof eventFormSchema>>({
+    resolver: zodResolver(eventFormSchema),
+    defaultValues: initialValues
+  })
+ 
+  async function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    let uploadedImageUrl = values.imageUrl;
+
+    if(files.length > 0) {
+      const uploadedImages = await startUpload(files)
+
+      if(!uploadedImages) {
+        return
       }
-  
-      if(type === 'Create') {
-        try {
-          const newEvent = await createEvent({
-            event: { ...values, imageUrl: uploadedImageUrl },
-            userId,
-            path: '/profile'
-          })
-  
-          if(newEvent) {
-            form.reset();
-            router.push(`/events/${newEvent._id}`)
-          }
-        } catch (error) {
-          console.log(error);
+
+      uploadedImageUrl = uploadedImages[0].url
+    }
+
+    if(type === 'Create') {
+      try {
+        const newEvent = await createEvent({
+          event: { ...values, imageUrl: uploadedImageUrl },
+          userId,
+          path: '/profile'
+        })
+
+        if(newEvent) {
+          form.reset();
+          router.push(`/events/${newEvent._id}`)
         }
-      }
-  
-      if(type === 'Update') {
-        if(!eventId) {
-          router.back()
-          return;
-        }
-  
-        try {
-          const updatedEvent = await updateEvent({
-            userId,
-            event: { ...values, imageUrl: uploadedImageUrl, _id: eventId },
-            path: `/events/${eventId}`
-          })
-  
-          if(updatedEvent) {
-            form.reset();
-            router.push(`/events/${updatedEvent._id}`)
-          }
-        } catch (error) {
-          console.log(error);
-        }
+      } catch (error) {
+        console.log(error);
       }
     }
-    return (
-        <Form {...form}>
+
+    if(type === 'Update') {
+      if(!eventId) {
+        router.back()
+        return;
+      }
+
+      try {
+        const updatedEvent = await updateEvent({
+          userId,
+          event: { ...values, imageUrl: uploadedImageUrl, _id: eventId },
+          path: `/events/${eventId}`
+        })
+
+        if(updatedEvent) {
+          form.reset();
+          router.push(`/events/${updatedEvent._id}`)
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
+  return (
+    <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5">
-      <div className="flex flex-col gap-5 md:flex-row">
-        <FormField
-          control={form.control}
-          name="title"
-          render={({ field }) => (
-            <FormItem className="w-full">
-              <FormControl>
-                <Input placeholder="Event title" {...field} className="input-field"/>
-              </FormControl>
-
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-         <FormField
-          control={form.control}
-          name="categoryId"
-          render={({ field }) => (
-            <FormItem className="w-full">
-              <FormControl>
-              <Dropdown onChangeHandler={field.onChange} value={field.value} />
-              </FormControl>
-
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input placeholder="Event title" {...field} className="input-field" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="categoryId"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Dropdown onChangeHandler={field.onChange} value={field.value} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </div>
 
         <div className="flex flex-col gap-5 md:flex-row">
@@ -205,7 +205,6 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
                       <p className="ml-3 whitespace-nowrap text-grey-600">Start Date:</p>
                       <DatePicker 
                         selected={field.value} 
-                        onChange={(date: Date) => field.onChange(date)} 
                         showTimeSelect
                         timeInputLabel="Time:"
                         dateFormat="MM/dd/yyyy h:mm aa"
@@ -236,7 +235,6 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
                       <p className="ml-3 whitespace-nowrap text-grey-600">End Date:</p>
                       <DatePicker 
                         selected={field.value} 
-                        onChange={(date: Date) => field.onChange(date)} 
                         showTimeSelect
                         timeInputLabel="Time:"
                         dateFormat="MM/dd/yyyy h:mm aa"
@@ -317,6 +315,7 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
             />
         </div>
 
+
         <Button 
           type="submit"
           size="lg"
@@ -328,10 +327,7 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
           ): `${type} Event `}</Button>
       </form>
     </Form>
-    )
+  )
 }
 
 export default EventForm
-
-  
-  
